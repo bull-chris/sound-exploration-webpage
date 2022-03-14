cd930603f4a128abd64c8584782d50a02ce483c6
Chris Bull <christopher.bull@dcmail.ca>
2022-03-14 05:14:10 -0400

    Added instructions and video overlay

--- js/app.js
+++ js/app.js
@@ -2,11 +2,24 @@ let popup = document.getElementById('popup')
 let howToPlayBtn = document.getElementById('instruction-button');
 let closeBtn = document.getElementById('popup-close');
 
+let videoDisplay = document.getElementById('video-display')
+let videoPlay = document.getElementById('soundButton');
+let closeVideoButton = document.getElementById('video-close');
+
 popup.classList.add('hidden')
+videoDisplay.classList.add('hidden');
 
 howToPlayBtn.addEventListener('click', () => {
     popup.classList.remove('hidden');
 })
 closeBtn.addEventListener('click', () => {
     popup.classList.add('hidden');
+})
+
+//video display play
+videoPlay.addEventListener('click', () => {
+    videoDisplay.classList.remove('hidden');
+})
+closeVideoButton.addEventListener('click', () => {
+    videoDisplay.classList.add('hidden');
 })
